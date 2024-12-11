4ebe05f5418b7f898b4745f5747ddfa28063b0b9
Valiant-Design <titilopeadebariuthman@gmail.com>
2024-12-11 12:20:37 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const App = () => {
         <div className="mx-4 sm:mx-[4%] bg-white ">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/allwork" element={<AllWork />} />
